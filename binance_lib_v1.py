f755c6be172a445561669aa19fe80431db4c5659
exgolden <isai_alejandro@outlook.com>
2024-12-28 15:25:24 -0600

    Hardened the error analyzers :construction:

--- binance_lib_v1.py
+++ binance_lib_v1.py
@@ -10,6 +10,7 @@ import time
 
 from cryptography.hazmat.primitives.hashes import SHA256
 from cryptography.hazmat.primitives.asymmetric.padding import PKCS1v15
+from requests.models import Request
 
 # Temporal imports for tesing
 # from cryptography.hazmat.primitives.serialization import load_pem_private_key
@@ -25,31 +26,54 @@ URL = "https://testnet.binance.vision/api"
     # private_key = load_pem_private_key(data=f.read(),
                                        # password=None)
 # General module
-def status_analyzer(status_code : int):
+def status_code_analyzer(response: dict) -> dict:
     """
-    Analyzes the HTTP status code and returns a descriptive message.
-    This function maps specific status codes to their corresponding error messages.
+    Analyzes the response and returns a descriptive message.
+    Returns a dictionary where the first element ('termination') is a boolean indicating
+    if the app must exit, and the second ('message') is the status description.
     Args:
-        status_code (int): Status code for the request.
+        response (dict): Response the API request.
+    Returns:
+        dict: Contains the termination status and a descriptive message:
+        ValueError: If the 'status_code' key is missing in the response.
+    Raises:
+        ValueError: If the status code is not present.
     """
-    match status_code:
+    if "status_code" not in response:
+        raise ValueError("The response does not contain a 'status_code' key.")
+    match response["status_code"]:
         case 403:
-            return "Web Application Limit has been violated."
-        case 409:
-            return "Cancel/Replace order partially succesfull."
+            return {"termination": True, "message": "Web Application Limit has been violated."}
         case 429:
-            return "Request limit broken."
+            return {"termination": True, "message": "Request limit broken."}
         case 418:
-            return "IP has been banned."
+            return {"termination": True, "message": "IP has been banned."}
         case _:
-            return f"Unhandled status code {status_code}: Further investigation needed."
+            return {"termination": False, "message": f"Unhandled status code {response['status_code']}: Further investigation needed."}
 
+def api_code_analyzer(response: dict) -> str:
+    """
+    Analyzes and returns a descriptive message for a specific API error code.
+    This function is called after confirming no critical HTTP status code has been returned.
+    Args:
+        response (dict): JSON response from the Binance API.
+    Returns:
+        str: Descriptive message for the error code.
+     Raises:
+        ValueError: If the code is not recognized or does not map to a known error type.
+    """
+    if "code" not in response:
+        raise ValueError("The response does not contain a 'code' key for error analysis.")
+    match response["code"]:
+        case -1121:
+            return "Invalid trading pair symbol."
+        case _:
+            return f"Unhandled code {response['code']}: Further investigation needed."
 
-def public_executor(params: dict, endpoint: str) -> dict:
+def public_executor(endpoint: str) -> dict:
     """
     Executes GET requests for a public endpoint (No key nor signature required).
     Args:
-        params (dict): Query parameters.
         endpoint (str): Relative URL to perform the query.
     Returns:
         dict: Parsed JSON response from the API.
@@ -58,59 +82,62 @@ def public_executor(params: dict, endpoint: str) -> dict:
     """
     final_endpoint = URL + endpoint
     try:
-        response = requests.get(final_endpoint, params=params)
+        response = requests.get(final_endpoint)
         if response.status_code == 200:
             return response.json()
-        else:
-            error_message = status_analyzer(response.status_code)
-            print(f"Error: {error_message}")
-        if response.status_code in [403, 418, 429]:
+        status_code_message = status_code_analyzer({"status_code": response.status_code})
+        print(f"Request failed with status {response.status_code}: {response.text}")
+        if status_code_message["termination"]:
             sys.exit(1)
-        raise RuntimeError(f"Request failed with status {response.status_code}: {response.text}")
-    except requests.RequestException as e:
-        raise RuntimeError(f"Error during request execution: {e}")
-  
-
-def private_executor(params: dict, endpoint: str) -> dict:
-    """
-    Executes a POST requests for a private endpoint (Key and signature required).
-    Args:
-        method (str): HTTP methos for the query.
-        params (dict): Query parameters.
-        endpoint (str): Relative URL to perform the query.
-    Returns:
-        dict: Parsed JSON response from the API.
-    Raises:
-        RuntimeError: If the request fails or returns a non-200 status code.
-    """
-    final_endpoint = URL + endpoint
-    timestamp = int(time.time() * 1000)
-    params["timestamp"] = timestamp
-    payload = "&".join([f"{param} = {value}" for param, value in params.items()])
-    signature =  base64.b64encode(
-        private_key.sign(
-            payload.encode('ASCII'),
-            PKCS1v15(),
-            SHA256()
-        )
-    )
-    params["signature"] = signature
-    headers = {
-        'X-MBX-APIKEY': API_KEY,
-    }
-    try:
-        response = requests.get(final_endpoint, headers=headers, params=params)
-        if response.status_code == 200:
-            return response.json()
-        else:
-            error_message = status_analyzer(response.status_code)
-            print(f"Error: {error_message}")
-        if response.status_code in [403, 418, 429]:
-            sys.exit(1)
-        raise RuntimeError(f"Request failed with status {response.status_code}: {response.text}")
+        json_response = response.json()
+        if "code" in json_response:
+            api_error_message = api_code_analyzer(json_response)
+            raise RuntimeError(f"API Error: {api_error_message}")
+        raise RuntimeError(f"Unhandled status code: {response.status_code}")
     except requests.RequestException as e:
         raise RuntimeError(f"Error during request execution: {e}")
 
+# def private_executor(params: dict, endpoint: str) -> dict:
+#     """
+#     Executes a POST requests for a private endpoint (Key and signature required).
+#     Args:
+#         method (str): HTTP methos for the query.
+#         params (dict): Query parameters.
+#         endpoint (str): Relative URL to perform the query.
+#     Returns:
+#         dict: Parsed JSON response from the API.
+#     Raises:
+#         RuntimeError: If the request fails or returns a non-200 status code.
+#     """
+#     final_endpoint = URL + endpoint
+#     timestamp = int(time.time() * 1000)
+#     params["timestamp"] = timestamp
+#     payload = "&".join([f"{param} = {value}" for param, value in params.items()])
+#     signature =  base64.b64encode(
+#         private_key.sign(
+#             payload.encode('ASCII'),
+#             PKCS1v15(),
+#             SHA256()
+#         )
+#     )
+#     params["signature"] = signature
+#     headers = {
+#         'X-MBX-APIKEY': API_KEY,
+#     }
+#     try:
+#         response = requests.get(final_endpoint, headers=headers, params=params)
+#         if response.status_code == 200:
+#             return response.json()
+#         else:
+#             error_message = status_analyzer(response.status_code)
+#             print(f"Error: {error_message}")
+#         if response.status_code in [403, 418, 429]:
+#             sys.exit(1)
+#         raise RuntimeError(f"Request failed with status {response.status_code}: {response.text}")
+#     except requests.RequestException as e:
+        raise RuntimeError(f"Error during request execution: {e}")
+
+# Healthcheck module
 def healthcheck() -> bool:
     """
     Verifies the health of the default endpoint by checking its response.
@@ -119,24 +146,36 @@ def healthcheck() -> bool:
               (returns an empty dictionary as the response), False otherwise.
     """
     try:
-        response = public_executor({}, "/v3/ping")
+        response = public_executor("/v3/ping")
         return response == {}
     except Exception as e:
         print(f"Healthcheck failed for default endpoint: {URL}: {e}")
         return False
 
+# Market data module
+# def get_current_price(symbol: str) -> dict:
+#     """
+#     Fetches the current price of a specific trading pair.
+#     Args:
+#         symbol (str): The trading pair symbol.
+#     Returns:
+#         dict: A dictionary containing the symbol and its current price.
+#     Raises:
+#         ValueError: If the response does not include expected keys.
+#         RuntimeError: If the request fails for any reason.
+#     """
+#     endpoint = f"/api/v3/ticker/price?symbol={symbol}"
+#     try:
+#         repsonse = public_executor({}, endpoint)
+#     except Exception as e:
+        raise e
+
+
+
 
 
 # Testing
-# print(healthcheck())
-
-
-
-
-
-
-
-
+print(healthcheck())
 
 
 
